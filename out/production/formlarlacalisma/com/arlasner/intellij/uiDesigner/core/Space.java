a950af7f66b9d08352e32e3e36bc72acf633db90
gorkem <113816295+gorkem1940@users.noreply.github.com>
2022-09-26 22:41:26 +0300

    Create Space.java

---
+++ out/production/formlarlacalisma/com/arlasner/intellij/uiDesigner/core/Space.java
@@ -0,0 +1,22 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.intellij.uiDesigner.core;
+
+import java.awt.Dimension;
+import javax.swing.JComponent;
+
+public class Spacer extends JComponent {
+    public Spacer() {
+    }
+
+    public Dimension getMinimumSize() {
+        return new Dimension(0, 0);
+    }
+
+    public final Dimension getPreferredSize() {
+        return this.getMinimumSize();
+    }
+}
